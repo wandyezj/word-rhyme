546117dafd53ada13cfe0131a0545bb4cb1ea037
Joe Wandyez <jowandy@microsoft.com>
2019-07-25 09:57:22 -0700

    update style

--- taskpane.js
+++ taskpane.js
@@ -1,11 +1,11 @@
 Office.onReady(info => {
     if (info.host === Office.HostType.Word) {
-        document.getElementById("sideload-msg").style.display = "none";
-        document.getElementById("app-body").style.display = "flex";
-        document.getElementById("run").onclick = run;
+        // document.getElementById("sideload-msg").style.display = "none";
+        // document.getElementById("app-body").style.display = "flex";
+        // document.getElementById("run").onclick = run;
     }
 });
-export async function run() {
+async function run() {
     return Word.run(async (context) => {
         // insert a paragraph at the end of the document.
         const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
